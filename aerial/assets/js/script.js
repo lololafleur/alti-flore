a9f0ed607318cccb5534a9fc14880eff94959ce8
lololafleur <sguitton2@gmail.com>
2017-11-15 22:00:51 +0100

    mise à jour aerial

---
+++ aerial/assets/js/script.js
@@ -0,0 +1,15 @@
+document.ready{function(
+	$('.collapse').collapse();
+	
+	$('#m_argo').click(function(){
+		$('#m-alti').collapse('hide');
+	
+	});
+	
+	$('#m_alti').click(function(){
+		$('#m-argo').collapse('hide');
+	
+	});
+
+
+)};
